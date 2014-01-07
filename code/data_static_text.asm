a8d8ce93257477758025c2da748808c433baba55
Daniel Keane <dkeane@innovadex.com>
2014-01-06 19:01:44 -0600

    Add menu for starting game

--- code/data_static_text.asm
+++ code/data_static_text.asm
@@ -1,5 +1,5 @@
 
 ; the two lines of text for color washer effect
 
-line1   !scr "keano game corp                         "
-line2   !scr "   proudly introduce...                 " 
+line1   !scr "       keano game corp                  "
+line2   !scr "         proudly presents...            " 
--- code/init_static_text.asm
+++ code/init_static_text.asm
@@ -2,23 +2,23 @@
 ; write the two line of text to screen center
 ;============================================================
 
-write_text lda $d018
-           ora #$0e       ; set chars location to $3800 for displaying the custom font
-           sta $d018      ; Bits 1-3 ($400+512bytes * low nibble value) of $d018 sets char location
-                          ; $400 + $200*$0E = $3800
-           lda $d016      ; turn off multicolor for characters
-           and #$ef       ; by cleaing Bit#4 of $D016
-           sta $d016
+write_text     lda $d018
+               ora #$0e       ; set chars location to $3800 for displaying the custom font
+               sta $d018      ; Bits 1-3 ($400+512bytes * low nibble value) of $d018 sets char location
+                              ; $400 + $200*$0E = $3800
+               lda $d016      ; turn off multicolor for characters
+               and #$ef       ; by cleaing Bit#4 of $D016
+               sta $d016
 
-		   ldx #$00         ; init X-Register with $00
-loop_text  lda line1,x      ; read characters from line1 table of text...
-           sta $0400,x      ; ...and store in screen ram near the center
-           lda line2,x      ; read characters from line1 table of text...
-           sta $0450,x      ; ...and put 2 rows below line1
+		           ldx #$00         ; init X-Register with $00
+loop_text      lda line1,x      ; read characters from line1 table of text...
+               sta $0450,x      ; ...and store in screen ram near the center
+               lda line2,x      ; read characters from line1 table of text...
+               sta $04a0,x      ; ...and put 2 rows below line1
 
-           inx 
-           cpx #$28         ; finished when all 40 cols of a line are processed
-           bne loop_text    ; loop if we are not done yet
+               inx 
+               cpx #$28         ; finished when all 40 cols of a line are processed
+               bne loop_text    ; loop if we are not done yet
 
                ldx #$00
 loop_top_menu  lda main_menu_top,x   ; do top 5 lines...
